d1d867c32c12bf24a90becff1296c72356d1c963
Hao <har@mailbox.org>
2022-12-10 09:11:12 +1100

    Initial Commit

---
+++ Tripset/TripsetApp.swift
@@ -0,0 +1,17 @@
+//
+//  TripsetApp.swift
+//  Tripset
+//
+//  Created by Hao on 10/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct TripsetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
